4121cbb94a57b5b865c438aa5e96a33963f5a51c
Abhijit A.M <abhijit13@gmail.com>
2017-04-09 16:50:34 +0530

    fixed whitespace in sctForm.js

--- sctForm.js
+++ sctForm.js
@@ -1,17 +1,17 @@
 function sctFormClose() {
-    document.getElementById("inputSCTForm").style.height = "0%";
-    document.getElementById("outerTable").style.display = "table";
-    document.getElementById("inputSCTForm").style.display= "none";
+	document.getElementById("inputSCTForm").style.height = "0%";
+	document.getElementById("outerTable").style.display = "table";
+	document.getElementById("inputSCTForm").style.display= "none";
 }
 function sctForm() {
-    document.getElementById("inputSCTForm").style.height = "auto";
-    document.getElementById("inputSCTForm").style.display = "block";
-    document.getElementById("outerTable").style.display = "none";
+	document.getElementById("inputSCTForm").style.height = "auto";
+	document.getElementById("inputSCTForm").style.display = "block";
+	document.getElementById("outerTable").style.display = "none";
 	var table = document.getElementById("sctTable");
 	table.innerHTML = ""; /* Required for recursive calls on delete */
 
-	/* Two ways of adding elements are used: createElement+appendChild  and
-	 * insertRow+insertCell 
+	/* Two ways of adding elements are used: createElement + appendChild and
+	 * insertRow + insertCell 
 	 */
 
 	/* ---- Adding Header Row -----------------------*/
@@ -102,7 +102,7 @@ function sctForm() {
 		var row = table.insertRow(count);
 		var cell = row.insertCell(0);
 		var centerTag = document.createElement("center");
-		centerTag.setAttribute("id", "center_"+count);
+		centerTag.setAttribute("id", "center_" + count);
 		var centerText = document.createTextNode(currSCT["sctId"]);
 		centerTag.appendChild(centerText);
 		cell.appendChild(centerTag);
@@ -110,7 +110,7 @@ function sctForm() {
 
 		cell = row.insertCell(-1);
 		var selectTag = document.createElement("select");
-		selectTag.setAttribute("id","class_"+count);
+		selectTag.setAttribute("id","class_" + count);
 		for (k in classTable) {
 			if(classTable[k]["classId"] == currSCT["classId"])
 				var tag = createOptionTag(classTable[k]["classId"], classTable[k]["classShortName"], true);		
@@ -119,11 +119,11 @@ function sctForm() {
 			selectTag.appendChild(tag);
 		}
 		cell.appendChild(selectTag);
-		//$("#class_"+i).select2();
+		//$("#class_" + i).select2();
 
 		cell = row.insertCell(-1);
 		var selectTag = document.createElement("select");
-		selectTag.setAttribute("id","subject_"+count);
+		selectTag.setAttribute("id","subject_" + count);
 		for (k in subject) {
 			if(subject[k]["batches"] == 1) /* don't show batchable subjects here */
 				continue;
@@ -134,12 +134,12 @@ function sctForm() {
 			selectTag.appendChild(tag);
 		}
 		cell.appendChild(selectTag);
-		//$("#subject_"+i).select2();
+		//$("#subject_" + i).select2();
 
 
 		cell = row.insertCell(-1);
 		var selectTag = document.createElement("select");
-		selectTag.setAttribute("id","teacher_"+ count);
+		selectTag.setAttribute("id","teacher_" + count);
 		for (k in teacher) {
 			if(teacher[k]["teacherId"] == currSCT["teacherId"])
 				var tag = createOptionTag(teacher[k]["teacherId"], teacher[k]["teacherShortName"], true);		
@@ -148,25 +148,25 @@ function sctForm() {
 			selectTag.appendChild(tag);
 		}
 		cell.appendChild(selectTag);
-		//$("#teacher_"+i).select2();
+		//$("#teacher_" + i).select2();
 
 		cell = row.insertCell(-1);
 		var button = document.createElement("button");
 		cell.appendChild(button);
-		button.value = "Update"; button.name = "sctUpdateButton_"+count;
+		button.value = "Update"; button.name = "sctUpdateButton_" + count;
 		var textNode = document.createTextNode("Update");
 		button.appendChild(textNode);
-		button.setAttribute("onclick","sctUpdate("+count+")");
-		button.setAttribute("id","sctUpdateButton_"+count);
+		button.setAttribute("onclick","sctUpdate(" + count + ")");
+		button.setAttribute("id","sctUpdateButton_" + count);
 
 		cell = row.insertCell(-1);
 		var button = document.createElement("button");
 		cell.appendChild(button);
-		button.value = "Delete"; button.name = "sctDeleteButton_"+count;
+		button.value = "Delete"; button.name = "sctDeleteButton_" + count;
 		var textNode = document.createTextNode("Delete");
 		button.appendChild(textNode);
-		button.setAttribute("onclick","sctDelete("+count+")");
-		button.setAttribute("id","sctDeleteButton_"+count);
+		button.setAttribute("onclick","sctDelete(" + count + ")");
+		button.setAttribute("id","sctDeleteButton_" + count);
 
 		count++;
 	}
@@ -176,6 +176,10 @@ function sctInsert() {
 	classId = document.getElementById("classAdd").value;	
 	subjectId = document.getElementById("subjectAdd").value;	
 	teacherId = document.getElementById("teacherAdd").value;	
+	if(classId == "" || subjectId == "" || teacherId == "") {
+		alert("Enter All Values");
+		return;
+	}
 	/* debug */
 	teacherShortName = search(teacher, "teacherId", teacherId)["teacherShortName"];
 	subjectShortName = search(subject, "subjectId", subjectId)["subjectShortName"];
@@ -203,21 +207,21 @@ function sctInsert() {
 	}
 	xhttp.open("POST", "timetable.php", false); // asynchronous
 	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhttp.send("reqType=sctInsert&subjectId="+subjectId+"&teacherId="+
-			teacherId+"&classId="+classId);
+	xhttp.send("reqType=sctInsert&subjectId=" + subjectId + "&teacherId=" +
+			teacherId + "&classId=" + classId);
 	
 }
 
 function sctUpdate(i) {
 	var row = i;
 	var classId, subjectId, teacheId, sctId;
-	classId = document.getElementById("class_"+row).value;	
-	subjectId = document.getElementById("subject_"+row).value;	
-	teacherId = document.getElementById("teacher_"+row).value;	
-	sctId = document.getElementById("center_"+row).childNodes[0].nodeValue;
-	document.getElementById("sctUpdateButton_"+row).childNodes[0].nodeValue = "Updating";
-	document.getElementById("sctDeleteButton_"+row).disabled = true;
-	document.getElementById("sctUpdateButton_"+row).disabled = true;
+	classId = document.getElementById("class_" + row).value;	
+	subjectId = document.getElementById("subject_" + row).value;	
+	teacherId = document.getElementById("teacher_" + row).value;	
+	sctId = document.getElementById("center_" + row).childNodes[0].nodeValue;
+	document.getElementById("sctUpdateButton_" + row).childNodes[0].nodeValue = "Updating";
+	document.getElementById("sctDeleteButton_" + row).disabled = true;
+	document.getElementById("sctUpdateButton_" + row).disabled = true;
 	/* debug */
 	teacherShortName = search(teacher, "teacherId", teacherId)["teacherShortName"];
 	subjectShortName = search(subject, "subjectId", subjectId)["subjectShortName"];
@@ -230,9 +234,9 @@ function sctUpdate(i) {
 			//alert("sct Row " + row + "Updated");		
 			response = JSON.parse(this.responseText);
 			if(response["Success"] == "True") {
-				document.getElementById("sctUpdateButton_"+i).childNodes[0].nodeValue = "Updated";
-				document.getElementById("sctDeleteButton_"+i).disabled = false;
-				document.getElementById("sctUpdateButton_"+i).disabled = false;
+				document.getElementById("sctUpdateButton_" + i).childNodes[0].nodeValue = "Updated";
+				document.getElementById("sctDeleteButton_" + i).disabled = false;
+				document.getElementById("sctUpdateButton_" + i).disabled = false;
 				subjectClassTeacher[row]["teacherId"] = teacherId;
 				subjectClassTeacher[row]["subjectId"] = subjectId;
 				subjectClassTeacher[row]["classId"] = classId;
@@ -240,51 +244,51 @@ function sctUpdate(i) {
 				sctForm();
 			}
 			else {
-				document.getElementById("sctUpdateButton_"+i).childNodes[0].nodeValue = "Update";
+				document.getElementById("sctUpdateButton_" + i).childNodes[0].nodeValue = "Update";
 				alert("sctId = " + sctId + ": Update Failed for Teacher: " + teacherShortName 
 								 + " Subject: " + subjectShortName + + " class: " + classShortName 
 									+ "\nError:\n" + response["Error"]);
 				document.getElementById("sctDeleteButton_"+i).disabled = false;
-				document.getElementById("sctUpdateButton_"+i).disabled = false;
+				document.getElementById("sctUpdateButton_" + i).disabled = false;
 				sctForm();
 			}
 		}
 	}
 	xhttp.open("POST", "timetable.php", false); // asynchronous
 	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhttp.send("reqType=sctUpdate&subjectId="+subjectId+"&teacherId="+
-			teacherId+"&classId="+classId+"&sctId="+sctId);
+	xhttp.send("reqType=sctUpdate&subjectId=" + subjectId + "&teacherId=" +
+			teacherId + "&classId=" + classId + "&sctId=" + sctId);
 	
 }
 function sctDelete(i) {
 	var row = i;
 	var classId, subjectId, teacheId, sctId;
 	var sure = confirm("This will delete all related timetable entries also\n"
-					  +"This can not be undone. \nAre you sure?");
+					 + "This can not be undone. \nAre you sure?");
 	if(sure != true)
 		return;
-	sctId = document.getElementById("center_"+row).childNodes[0].nodeValue;
-	document.getElementById("sctDeleteButton_"+row).childNodes[0].nodeValue = "Deleting";
-	document.getElementById("sctDeleteButton_"+row).disabled = true;
-	document.getElementById("sctUpdateButton_"+row).disabled = true;
+	sctId = document.getElementById("center_" + row).childNodes[0].nodeValue;
+	document.getElementById("sctDeleteButton_" + row).childNodes[0].nodeValue = "Deleting";
+	document.getElementById("sctDeleteButton_" + row).disabled = true;
+	document.getElementById("sctUpdateButton_" + row).disabled = true;
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if(this.readyState == 4 && this.status == 200) {
 			response = JSON.parse(this.responseText);
 			if(response["Success"] == "True") {
-				document.getElementById("sctDeleteButton_"+row).value = "Delete"
+				document.getElementById("sctDeleteButton_" + row).value = "Delete"
 				subjectClassTeacher.splice(i - 2, 1);
 				fillTable2(true);
 				sctForm();
 			} else {
 				alert("sct " + sctId + ": Deletion Failed.\nError:\n" + response["Error"]);
-				document.getElementById("sctDeleteButton_"+row).value = "Delete"
-				document.getElementById("sctUpdateButton_"+row).disabled = false;
-				document.getElementById("sctDeleteButton_"+row).childNodes[0].nodeValue = "Can't Delete";
+				document.getElementById("sctDeleteButton_" + row).value = "Delete"
+				document.getElementById("sctUpdateButton_" + row).disabled = false;
+				document.getElementById("sctDeleteButton_" + row).childNodes[0].nodeValue = "Can't Delete";
 			}
 		}
 	}
 	xhttp.open("POST", "timetable.php", true); // asynchronous
 	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhttp.send("reqType=sctDelete&sctId="+sctId);
+	xhttp.send("reqType=sctDelete&sctId=" + sctId);
 }
